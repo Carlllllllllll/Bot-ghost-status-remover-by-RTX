05298133d28915197e7567114c5e06cd00cb09e1
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-02-06 12:09:21 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["𝐏𝐥𝐚𝐲𝐢𝐧𝐠 𝐌𝐮𝐬𝐢𝐜 🎵"];
 
 
 let currentIndex = 0;
